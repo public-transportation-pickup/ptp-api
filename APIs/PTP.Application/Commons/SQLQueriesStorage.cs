86255c78ca4a39b33f65482d578881dcf4d0237c
KIEUMINHHIEU <minhhieutb321@gmail.com>
2024-03-18 00:57:00 +0700

    Edit

--- APIs/PTP.Application/Commons/SQLQueriesStorage.cs
+++ APIs/PTP.Application/Commons/SQLQueriesStorage.cs
@@ -1,15 +1,15 @@
 namespace PTP.Application.Commons;
 public static class SqlQueriesStorage
 {
-    public const string GET_STATION_BY_ROUTEVARID =@"
-                                            SELECT s.Id, s.[Name], rs.[Index]
+    public const string GET_STATION_BY_ROUTEVARID = @"
+                                            SELECT s.Id, s.[Name], rs.[Index], s.[Latitude], s.[Longitude]
                                             FROM RouteStation rs
                                             INNER JOIN Station s
                                             ON rs.StationId = s.Id
                                             WHERE rs.RouteVarId = @routeVarId
                                             AND s.ISDELETED = 0
                                             ORDER BY rs.[Index]";
-	public const string GET_ALL_USER = @"
+    public const string GET_ALL_USER = @"
                                         SELECT u.Id, FullName, 
                                         [Email], PhoneNumber, 
                                         DateOfBirth, r.[Name] AS RoleName
@@ -17,7 +17,7 @@ public static class SqlQueriesStorage
                                         Role r
                                         ON u.RoleID = r.Id
                                         WHERE u.IsDeleted = 0";
-	public const string GET_USER_BY_ID = @"
+    public const string GET_USER_BY_ID = @"
                                         SELECT u.Id, FullName, 
                                         [Email], PhoneNumber, 
                                         DateOfBirth, r.[Name] AS RoleName
@@ -26,10 +26,10 @@ public static class SqlQueriesStorage
                                         ON u.RoleID = r.Id
                                         WHERE u.Id = @id
                                         AND u.IsDeleted = 0";
-	/// <summary>
-	/// Get RouteStation By RouteId and RouteVarId
-	/// </summary>
-	public const string GET_ROUTE_STATION_BY_PARENT_ID = @"
+    /// <summary>
+    /// Get RouteStation By RouteId and RouteVarId
+    /// </summary>
+    public const string GET_ROUTE_STATION_BY_PARENT_ID = @"
                                         SELECT rs.Id, rs.[Index], s.Latitude, s.Longitude, 
                                         rs.DistanceFromStart, rs.DistanceToNext, rs.DurationFromStart, 
                                         rs.DurationToNext, s.[Id] AS StationId, s.[Name] AS StationName
@@ -55,12 +55,12 @@ public static class SqlQueriesStorage
                                         AND rs.IsDeleted = 0
                                         ORDER BY [Index]";
 
-	/// <summary>
-	/// Get All Trips by
-	/// <param name="id">RouteId</param>
-	/// <param name="routeVarId">RouteVarId</param>
-	/// </summary>
-	public const string GET_TRIPS_BY_PARENTS_ID = @"
+    /// <summary>
+    /// Get All Trips by
+    /// <param name="id">RouteId</param>
+    /// <param name="routeVarId">RouteVarId</param>
+    /// </summary>
+    public const string GET_TRIPS_BY_PARENTS_ID = @"
                                                     SELECT t.Id,tt.ApplyDates, t.[Name], t.StartTime, 
                                                     t.EndTime, t.[Status] FROM TimeTables tt JOIN Trip t
                                                     ON t.TimeTableId = tt.Id
@@ -68,29 +68,29 @@ public static class SqlQueriesStorage
                                                     AND t.IsDeleted = 0
                                                     AND RouteVarId = @routeVarId";
 
-	/// <summary>
-	/// Get Trip By 
-	/// <param name="id">TripId</param>
-	/// </summary>                         
-	public const string GET_TRIP_BY_ID = @"SELECT t.Id,tt.ApplyDates, t.[Name], t.StartTime, t.EndTime, t.[Status] 
+    /// <summary>
+    /// Get Trip By 
+    /// <param name="id">TripId</param>
+    /// </summary>                         
+    public const string GET_TRIP_BY_ID = @"SELECT t.Id,tt.ApplyDates, t.[Name], t.StartTime, t.EndTime, t.[Status] 
                                                     FROM TimeTables tt JOIN Trip t
                                                     ON t.TimeTableId = tt.Id
                                                     WHERE t.Id = @id AND t.IsDeleted = 0";
 
-	/// <summary>
-	/// Get Trips By 
-	/// <param name="timeTableId">TimeTableId</param> 
-	/// </summary>
-	public const string GET_TRIP_BY_TIMETABLEID = @"SELECT t.Id,tt.ApplyDates, t.[Name], t.StartTime, t.EndTime, t.[Status] 
+    /// <summary>
+    /// Get Trips By 
+    /// <param name="timeTableId">TimeTableId</param> 
+    /// </summary>
+    public const string GET_TRIP_BY_TIMETABLEID = @"SELECT t.Id,tt.ApplyDates, t.[Name], t.StartTime, t.EndTime, t.[Status] 
                                                     FROM TimeTables tt JOIN Trip t
                                                     ON t.TimeTableId = tt.Id
                                                     WHERE tt.Id = @timeTableId AND t.IsDeleted = 0
                                                     ORDER BY t.StartTime";
 
-	/// <summary>
-	/// Get Trip Schedule by TripId
-	/// </summary>
-	public const string GET_TRIP_SCHEDULE_BY_ID = @"SELECT rs.[Index] ,rs.DistanceFromStart, rs.DistanceToNext, rs.DurationFromStart, 
+    /// <summary>
+    /// Get Trip Schedule by TripId
+    /// </summary>
+    public const string GET_TRIP_SCHEDULE_BY_ID = @"SELECT rs.[Index] ,rs.DistanceFromStart, rs.DistanceToNext, rs.DurationFromStart, 
                                                     rs.DurationToNext, s.[Name] AS StationName,
                                                     DATEADD(MINUTE, rs.DurationFromStart, PARSE(t.StartTime AS time)) AS ArrivalTime,
                                                     rs.StationId AS StationId,
@@ -104,10 +104,10 @@ public static class SqlQueriesStorage
                                                     WHERE t.Id = @id
                                                     AND t.IsDeleted = 0
                                                     ORDER BY [Index]";
-	/// <summary>
-	/// Get Timetable by routeId and RouteVarId
-	/// </summary>
-	public const string GET_TIMETABLE_BY_PARID = @"
+    /// <summary>
+    /// Get Timetable by routeId and RouteVarId
+    /// </summary>
+    public const string GET_TIMETABLE_BY_PARID = @"
                                                 SELECT tt.Id, TimeTableId, tt.IsCurrent, 
                                                 tt.ApplyDates, tt.RouteId, 
                                                 tt.RouteVarId FROM [TimeTables] tt 
@@ -119,10 +119,10 @@ public static class SqlQueriesStorage
                                                 AND tt.RouteId = @routeId
                                                 AND tt.IsDeleted = 0
                                                 ORDER BY TimeTableId";
-	/// <summary>
-	/// Get All Route Vars by RouteId
-	/// </summary>
-	public const string GET_ROUTE_VARS_BY_ROUTE_ID = @"
+    /// <summary>
+    /// Get All Route Vars by RouteId
+    /// </summary>
+    public const string GET_ROUTE_VARS_BY_ROUTE_ID = @"
                                                 SELECT rv.Id, RouteVarName, 
                                                 RouteVarShortName, StartStop, EndStop,
                                                 OutBound, RunningTime, r.RouteId, rv.Distance
@@ -130,10 +130,10 @@ public static class SqlQueriesStorage
                                                 ON rv.RouteId = r.Id
                                                 WHERE rv.RouteId = @routeId AND rv.IsDeleted = 0
                                                 ORDER BY rv.RouteVarId";
-	/// <summary>
-	/// CommandText của GET TIMETABLE BY ID
-	/// </summary>
-	public const string GET_TIMETABLE_BY_ID = @"SELECT * FROM TimeTables 
+    /// <summary>
+    /// CommandText của GET TIMETABLE BY ID
+    /// </summary>
+    public const string GET_TIMETABLE_BY_ID = @"SELECT * FROM TimeTables 
                                                 WHERE Id = @id 
                                                 AND t.IsDeleted = 0";
 }
