dea71a1713411e4791041a1216f0022a2e7d2063
quangtm <quangtm0152@gmail.com>
2024-03-24 23:57:46 +0700

    fixing

--- APIs/PTP.Application/ViewModels/MongoDbs/Carts/CartItemCreateModel.cs
+++ APIs/PTP.Application/ViewModels/MongoDbs/Carts/CartItemCreateModel.cs
@@ -3,7 +3,7 @@ namespace PTP.Application.ViewModels.MongoDbs.Carts;
 public class CartItemCreateModel
 {
     
-    public Guid ProductInMenuId { get; set; } = Guid.Empty;
+    public Guid ProductMenuId { get; set; } = Guid.Empty;
     public int Quantity { get; set; } = 0;
     public string Note { get; set; } = string.Empty;
 }
