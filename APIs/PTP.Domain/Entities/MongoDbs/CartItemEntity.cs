b3509db36a2e2482e6e8abe8ea7757c8983b9bdf
quangtm <quangtm0152@gmail.com>
2024-03-26 18:40:02 +0700

    fixing

--- APIs/PTP.Domain/Entities/MongoDbs/CartItemEntity.cs
+++ APIs/PTP.Domain/Entities/MongoDbs/CartItemEntity.cs
@@ -8,5 +8,6 @@ public class CartItemEntity
     public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
     public decimal ActualPrice { get; set; }
     public int Quantity { get; set; }
+    public string Note { get; set; } = string.Empty;
     public Guid ProductMenuId { get; set; }
 }
