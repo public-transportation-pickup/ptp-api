00eaa58032258dfe2550d73ff0d8f48c206c2a56
quangtm <quangtm0152@gmail.com>
2024-03-28 02:31:57 +0700

    s

---
+++ APIs/PTP.Application/ViewModels/MongoDbs/Carts/CartItemUpdateModel.cs
@@ -0,0 +1,9 @@
+namespace PTP.Application.ViewModels.MongoDbs.Carts;
+public class CartItemUpdateModel
+{
+    public Guid ProductMenuId { get; set; }
+    public int Quantity { get; set; }
+    public decimal ActualPrice { get; set; }
+    public string ImageURL { get; set; } = string.Empty;
+    public string Note { get; set; } = string.Empty;
+}
