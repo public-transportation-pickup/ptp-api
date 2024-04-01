c1a9d4f1a90aeba64a99cc6612058714affc2a85
quangtm <quangtm0152@gmail.com>
2024-04-01 14:06:02 +0700

    fixing

--- APIs/PTP.Application/ViewModels/Orders/OrderViewModel.cs
+++ APIs/PTP.Application/ViewModels/Orders/OrderViewModel.cs
@@ -18,6 +18,7 @@ public class OrderViewModel
     public string StationAddress { get; set; } = default!;
     public Guid StoreId { get; set; }
     public string StoreName { get; set; } = default!;
+    public string StoreAddress {get; set;} = string.Empty;
     public string StorePhoneNumber { get; set; } = default!;
     public Guid PaymentId { get; set; }
     public string PaymentType { get; set; } = default!;
