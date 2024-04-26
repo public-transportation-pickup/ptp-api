5640d2c76b16c90933389b86ca513e4f9830292c
quangtm <quangtm0152@gmail.com>
2024-04-27 03:00:15 +0700

    ss

--- APIs/PTP.Application/Features/Trips/Queries/GetTripByCoordinateQuery.cs
+++ APIs/PTP.Application/Features/Trips/Queries/GetTripByCoordinateQuery.cs
@@ -3,6 +3,7 @@ using MongoDB.Driver.Linq;
 using PTP.Application.Features.Trips.Models;
 using PTP.Application.IntergrationServices.Interfaces;
 using PTP.Application.Services.RouteStations;
+using PTP.Application.Utilities;
 using PTP.Application.ViewModels.RouteStations;
 using PTP.Application.ViewModels.Trips;
 
@@ -28,8 +29,10 @@ public class GetTripByUserLocation : IRequest<TripCoordinateResponseModel?>
         }
         public async Task<TripCoordinateResponseModel?> Handle(GetTripByUserLocation request, CancellationToken cancellationToken)
         {
-            var timeTable = (await unitOfWork.TimeTableRepository.WhereAsync(x => x.RouteVarId == request.RouteVarId && x.IsCurrent)
-                ?? throw new Exception("Not found timetable")).First();
+            var timeTables = await unitOfWork.TimeTableRepository.WhereAsync(x => x.RouteVarId == request.RouteVarId)
+                ?? throw new Exception("Not found timetable");
+            var timeTable = timeTables.FirstOrDefault(x => x.ApplyDates.Contains(DateTime.Today.DayOfWeek.ConvertToDateApplys()))
+                ?? throw new Exception("Not support for today, " + DateTime.Today.DayOfWeek);
 
             var currentCoordinate = new { request.Latitude, request.Longitude };
 
@@ -67,11 +70,33 @@ public class GetTripByUserLocation : IRequest<TripCoordinateResponseModel?>
                 //     .OrderBy(x => x.DistanceFromStart - distanceToStart)
                 //     .Take(2)
                 //     .MaxBy(x => x.Index);
-                var nextStation = routeStations
-                    .OrderBy(x => Math.Abs(x.Latitude - currentCoordinate.Latitude))
-                    .ThenBy(x => Math.Abs(x.Latitude - currentCoordinate.Longitude))
-                    .Take(2)
-                    .MaxBy(x => x.Index);
+                // var nextStation = routeStations
+                //     .OrderBy(x => Math.Abs(x.Latitude - currentCoordinate.Latitude))
+                //     .ThenBy(x => Math.Abs(x.Latitude - currentCoordinate.Longitude))
+                //     .Take(2)
+                //     .MaxBy(x => x.Index);
+
+
+                var groupStations = routeStations
+                        .GroupBy(x => Math.Pow((double)(currentCoordinate.Latitude - x.Latitude), 2) + Math.Pow((double)(currentCoordinate.Longitude - x.Longitude), 2))
+                      .OrderBy(x => x.Key)
+                      .Take(2);
+                int maxIndex = 0;
+                foreach (var item in groupStations)
+                {
+                    foreach (var station in item)
+                    {
+                        if (station.Index > maxIndex)
+                        {
+                            maxIndex = station.Index;
+                        }
+                    }
+                }
+                var nextStation = routeStations.FirstOrDefault(x => x.Index == maxIndex);
+
+
+
+
 
                 var eta = DateTime.Now.AddMinutes(duration);
 
@@ -94,25 +119,7 @@ public class GetTripByUserLocation : IRequest<TripCoordinateResponseModel?>
 
         }
 
-        private static int FindLowerBoundIndex(decimal[,] points,
-            decimal lat,
-            decimal lon)
-        {
-            int left = 0;
-            int right = points.GetLength(0) - 1;
 
-            while (left <= right)
-            {
-                int mid = left + (right - left) / 2;
-
-                if (points[mid, 0] < lat || (points[mid, 0] == lat && points[mid, 1] < lon))
-                    left = mid + 1;
-                else
-                    right = mid - 1;
-            }
-
-            return right;
-        }
         private static int FindUpperBoundIndex(decimal[,] points,
             decimal lat,
             decimal lon)
