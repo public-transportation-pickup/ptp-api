ef26f3a8104fcbe049ec23ae27c19938c6b74ddc
quangtm <quangtm0152@gmail.com>
2024-04-30 13:10:16 +0700

    ss

--- APIs/PTP.Application/Features/Timetables/Commands/CreateTimetableCommand.cs
+++ APIs/PTP.Application/Features/Timetables/Commands/CreateTimetableCommand.cs
@@ -1,6 +1,8 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PTP.Application.GlobalExceptionHandling.Exceptions;
 using PTP.Application.ViewModels.Timetables;
 using PTP.Domain.Entities;
 
@@ -31,6 +33,13 @@ public class CreateTimetableCommand : IRequest<List<TimeTable>?>
         }
         public async Task<List<TimeTable>?> Handle(CreateTimetableCommand request, CancellationToken cancellationToken)
         {
+            foreach(var item in request.Models) 
+            {
+                if(await unitOfWork.TimeTableRepository.FirstOrDefaultAsync(x => x.RouteId == item.RouteId && x.RouteVarId == item.RouteVarId) is not null)
+                {
+                    throw new BadRequestException($"Đã có thời khoá biểu tồn tại RouteVarId :{item.RouteVarId}");
+                }
+            }
             var timetables = unitOfWork.Mapper.Map<IEnumerable<TimeTable>>(request.Models);
             await unitOfWork.TimeTableRepository.AddRangeAsync(timetables.ToList());
             await unitOfWork.SaveChangesAsync();
